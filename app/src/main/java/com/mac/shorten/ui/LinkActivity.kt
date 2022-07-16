d84471cc7ca83ecdd81ea4156475a1c018228d03
supermac <m.abouchaker@totersapp.com>
2022-07-16 18:26:02 +0300

    add activity

---
+++ app/src/main/java/com/mac/shorten/ui/LinkActivity.kt
@@ -0,0 +1,12 @@
+package com.mac.shorten.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mac.shorten.R
+
+class LinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_link)
+    }
+}
